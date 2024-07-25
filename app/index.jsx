b525bc5a742a16d2464fa195688ce1a6f9394e16
Rest <code.rest.dev@gmail.com>
2024-07-24 21:21:49 -0400

    0.0.1 Creando el setup del proyecto 1/2

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Hola a todos , yo soy Aura</Text>
+      {/* <StatusBar style="auto" /> */}
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to profile
+      </Link>
+    </View>
+  );
+}
+
+
---
+++ app/profile.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Text>Este es la pantalla de profile</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
